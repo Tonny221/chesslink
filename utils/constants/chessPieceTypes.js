f77f26c0219bb57253c284c6788dff4ec2235bce
Ariel Benichou <videojr3000@gmail.com>
2021-08-20 00:45:40 +0300

    added constants and index

---
+++ utils/constants/chessPieceTypes.js
@@ -0,0 +1,6 @@
+export const KING = "KING";
+export const QUEEN = "QUEEN";
+export const ROOK = "ROOK";
+export const BISHOP = "BISHOP";
+export const KNIGHT = "KNIGHT";
+export const PAWN = "PAWN";
