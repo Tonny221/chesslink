37e6836a991a7ffb6acdbefa05800a1ee3f4c669
Ariel Benichou <videojr3000@gmail.com>
2021-08-25 22:09:18 +0300

    use custom App for next margin: 0 and imported Roboto font from google fonts

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function MyApp({ Component, pageProps }) {
+  //TODO: put here the Background color
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
