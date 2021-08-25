97ed3a9dee2223d9d66e7193089c39c294f70601
Ariel Benichou <videojr3000@gmail.com>
2021-08-25 22:07:43 +0300

    switch to use chess.js

--- components/ChessBoard/ChessBoard.js
+++ components/ChessBoard/ChessBoard.js
@@ -1,30 +1,28 @@
 import { React } from "react";
 import ChessPiece from "@components/ChessPiece/ChessPiece";
 import { Row, Tile, BoardContainer } from "./ChessBoard.styles";
-import { getWidth, isTileBlack } from "@services/chess/chess.service";
+import { isTileBlack } from "@services/chess/chess.service";
 
-const ChessBoard = ({ chessData, movePiece }) => {
+const ChessBoard = ({ chess, movePiece, getAllMoves }) => {
+  const size = 4;
   return (
     <BoardContainer>
-      {chessData.board.map((row, rowIndex) => {
+      {chess.board().map((row, rowIndex) => {
         return (
           <Row key={rowIndex}>
             {row.map((chessPiece, colIndex) => {
               return (
                 <Tile
-                  key={rowIndex * getWidth(chessData) + colIndex}
-                  isBlack={isTileBlack(rowIndex, colIndex)}
+                  key={rowIndex * row.length + colIndex}
+                  color={
+                    isTileBlack(rowIndex, colIndex) ? "rebeccapurple" : "gold"
+                  }
+                  size={size}
                 >
                   {chessPiece && (
                     <ChessPiece
                       type={chessPiece.type}
                       color={chessPiece.color}
-                      onClick={() => {
-                        movePiece(
-                          [rowIndex, colIndex],
-                          [rowIndex + 1, colIndex]
-                        );
-                      }}
                     />
                   )}
                 </Tile>
