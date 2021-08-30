9a5dcdc076b634816727bf5b3da0f64286b16f7e
Ariel Benichou <videojr3000@gmail.com>
2021-08-30 18:54:01 +0300

    fixed the tests

--- services/chess/chess.service.js
+++ services/chess/chess.service.js
@@ -1,6 +1,8 @@
+import { isEven, isOdd } from "@utils/helpers/math";
+
 const isTileBlack = (row, col, flipColors = false) => {
-  const isBlack = row % 2 !== 0 ? col % 2 === 0 : col % 2 !== 0;
-  return flipColors ? !isBlack : isBlack;
+  const isBlack = isEven(row) ? isOdd(col) : isEven(col);
+  return flipColors ? isBlack : !isBlack;
 };
 
 const isTileWhite = (row, col, flipColors = false) => {
